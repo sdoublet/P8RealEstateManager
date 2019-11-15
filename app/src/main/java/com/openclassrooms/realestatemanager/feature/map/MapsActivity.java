eb48f89621b329f049248d2259064765666fa11c
sdoublet <doubletsebastien@sfr.fr>
2019-11-15 10:44:33 +0100

    add easyperm and set geolocation

--- app/src/main/java/com/openclassrooms/realestatemanager/feature/map/MapsActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/feature/map/MapsActivity.java
@@ -1,20 +1,41 @@
 package com.openclassrooms.realestatemanager.feature.map;
 
+import android.Manifest;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.fragment.app.FragmentActivity;
 
-import android.os.Bundle;
-
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 import com.openclassrooms.realestatemanager.R;
+
+import pub.devrel.easypermissions.EasyPermissions;
+
+import static android.content.ContentValues.TAG;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String[] perms = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+    public static final String API_KEY = String.valueOf(R.string.google_maps_key);
+    private static final int REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 12f;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+
     private GoogleMap mMap;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +45,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
     }
 
 
@@ -40,9 +62,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        displayCurrentLocation(googleMap);
     }
+
+    private void displayCurrentLocation(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (EasyPermissions.hasPermissions(getApplicationContext(), perms)) {
+            mMap.setMyLocationEnabled(true);
+            Task locationResult = fusedLocationProviderClient.getLastLocation();
+            locationResult.addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Location currentLocation = (Location) task.getResult();
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM));
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()));
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                    markerOptions.title("My position");
+                    marker = mMap.addMarker(markerOptions);
+                    Log.e("location", "latitude = " + currentLocation.getLatitude());
+                    Log.e("location", "longitude = " + currentLocation.getLongitude());
+                } else {
+                    Log.d(TAG, "Current location is null. Using defaults.");
+                    Log.e(TAG, "Exception: %s", task.getException());
+                    Toast.makeText(getApplicationContext(), "unable to get current location", Toast.LENGTH_LONG).show();
+                    LatLng mDefaultLocation = new LatLng(-34, 151);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                    mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                }
+            });
+
+        } else {
+            EasyPermissions.requestPermissions(this, "you must have permission", REQUEST_CODE, perms);
+        }
+    }
+
 }

d094a3db80a2b3d2107c0ebb4325b86e394c1828
sdoublet <doubletsebastien@sfr.fr>
2019-11-15 17:27:06 +0100

    add all classes for geocoding api

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/api/ApiGeocoding.java
@@ -0,0 +1,34 @@
+package com.openclassrooms.realestatemanager.api;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+
+public class ApiGeocoding {
+
+    @SerializedName("results")
+    @Expose
+    private List<Result> results = null; //careful about Result import
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}

bc8c516e41c38d6bbfcaa497fcbe0b9a4bf1ad6a
sdoublet <doubletsebastien@sfr.fr>
2019-11-21 16:23:25 +0100

    add ViewModelFactory

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/injections/ViewModelFactory.java
@@ -0,0 +1,29 @@
+package com.openclassrooms.realestatemanager.injections;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.openclassrooms.realestatemanager.repositories.EstateDataRepository;
+import com.openclassrooms.realestatemanager.repositories.UserDataRepository;
+
+import java.util.concurrent.Executor;
+
+public class ViewModelFactory implements ViewModelProvider.Factory {
+
+    private final EstateDataRepository estateDataRepository;
+    private final UserDataRepository userDataRepository;
+    private final Executor executor;
+
+    public ViewModelFactory(EstateDataRepository estateDataRepository, UserDataRepository userDataRepository, Executor executor) {
+        this.estateDataRepository = estateDataRepository;
+        this.userDataRepository = userDataRepository;
+        this.executor = executor;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return null;
+    }
+}

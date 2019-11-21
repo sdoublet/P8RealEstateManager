e2fe1338eb685cde7593886ee3432efa3eacd504
sdoublet <doubletsebastien@sfr.fr>
2019-11-21 16:29:50 +0100

    add injections

--- app/src/main/java/com/openclassrooms/realestatemanager/injections/Injection.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/injections/Injection.java
@@ -1,4 +1,34 @@
 package com.openclassrooms.realestatemanager.injections;
 
+import android.content.Context;
+
+import com.openclassrooms.realestatemanager.database.database.RoomDb;
+import com.openclassrooms.realestatemanager.repositories.EstateDataRepository;
+import com.openclassrooms.realestatemanager.repositories.UserDataRepository;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class Injection {
+
+    public static EstateDataRepository provideEstateDataSource(Context context){
+        RoomDb roomDb = RoomDb.getInstance(context);
+        return new EstateDataRepository(roomDb.estateDao());
+    }
+
+    public static UserDataRepository provideUserDataSource(Context context){
+        RoomDb roomDb = RoomDb.getInstance(context);
+        return new UserDataRepository(roomDb.userDao());
+    }
+
+    public static Executor provideExecutor(){
+        return Executors.newSingleThreadExecutor();
+    }
+
+    public static ViewModelFactory provideViewModelFactory(Context context){
+        EstateDataRepository estateDataRepository = provideEstateDataSource(context);
+        UserDataRepository userDataRepository = provideUserDataSource(context);
+        Executor executor = provideExecutor();
+        return new ViewModelFactory(estateDataRepository, userDataRepository, executor);
+    }
 }

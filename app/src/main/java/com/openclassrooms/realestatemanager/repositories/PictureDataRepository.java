2ad5271ebf5ae90bb33e835a5e3e1da21c965003
sdoublet <doubletsebastien@sfr.fr>
2019-12-04 13:08:42 +0100

    add Picture repository

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/repositories/PictureDataRepository.java
@@ -0,0 +1,26 @@
+package com.openclassrooms.realestatemanager.repositories;
+
+import androidx.lifecycle.LiveData;
+
+import com.openclassrooms.realestatemanager.database.dao.PictureDao;
+import com.openclassrooms.realestatemanager.models.Picture;
+
+import java.util.List;
+
+public class PictureDataRepository {
+
+    private final PictureDao pictureDao;
+
+    public PictureDataRepository(PictureDao pictureDao) {
+        this.pictureDao = pictureDao;
+    }
+
+    //--------CREATE----------
+    public void createPicture(Picture picture){pictureDao.insertPicture(picture);}
+
+    //--------DELETE-----------
+    public void deletePicture(Picture picture){pictureDao.deletePicture(picture);}
+
+    //-------GET---------------
+    public LiveData<List<Picture>> getAllPictureFromEstateId(long estateId){return this.pictureDao.getPicture(estateId);}
+}
